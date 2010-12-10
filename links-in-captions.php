228708826703ec0a7f4993c480bfc604ccbf53c9
katzwebdesign <katzwebdesign@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-10 02:56:50 +0000

    Version 1.1 is better!
    git-svn-id: https://plugins.svn.wordpress.org/links-in-captions/trunk@321181 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- links-in-captions.php
+++ links-in-captions.php
@@ -4,7 +4,7 @@ Plugin Name: Links in Captions
 Plugin URI: http://www.seodenver.com/lottery/
 Description: Easily add links to image captions in the WordPress editor.
 Author: Katz Web Services, Inc.
-Version: 1.0
+Version: 1.1
 Author URI: http://www.katzwebservices.com
 */
 
@@ -25,54 +25,115 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
+add_shortcode('add_caption_link', 'add_link_to_caption_shortcode_shortcode');
 add_filter('img_caption_shortcode', 'add_link_to_caption_shortcode', true, 3);
 // This function is taken from wp-includes/media.php
+function add_link_to_caption_replace_quotes2($string, $type = 'add') {
+	$out = '}';
+		if(is_array($string)) { $text = $string[1]; } else { $text = $string; }
+		$text = str_replace("'",'%%squote%%', $text);
+		$text = str_replace('&#39;','%%squote%%', $text);
+		$text = str_replace('&quot;','%%dquote%%', $text);
+		$text = str_replace('"','%%dquote%%', $text);
+	
+	$out .= $text.'{/link}';
+	return $out;
+}
+function add_link_to_caption_replace_quotes($string, $type = 'add') {
+	if($type == 'add') {
+		if(is_array($string)) { $text = $string[1]; } else { $text = $string; }
+		$text = str_replace("'",'%%squote%%', $text);
+		$text = str_replace('&#39;','%%squote%%', $text);
+		$text = str_replace('&quot;','%%dquote%%', $text);
+		$text = str_replace('"','%%dquote%%', $text);
+		return $text; 
+	} else {
+		$string = str_replace('%%squote%%', '\'', $string);
+		$string = str_replace('%%dquote%%', '"', $string);
+		return $string;
+	}
+}
 function add_link_to_caption_shortcode($empty, $attr, $content) {
+	$caption = '';
+	if(!isset($attr['caption'])) {
+		// Used double quotes for link
+		$match = false; $string = $key = ''; $attrCopy = $attr; 
+		foreach($attr as $key => $att) {
+			if(preg_match('/\{link/ism', $att)) { $match = true; }
+			if(!is_numeric($key)) {
+				$attrCopy[$key] = $key.'="'.add_link_to_caption_replace_quotes($att).'"';
+			}
+		}
+
+		if($match) {
+			$string = implode(' ', $attrCopy);
+			$string = preg_replace('/(href|url|rel|target|title|text)=(?:\s+)?(?:\'|&#39;)(.*?)(?:\'|&#39;)/ism', '$1=%%squote%%$2%%squote%%', $string);
+			$string = preg_replace('/(href|url|rel|target|title|text)=(?:\s+)?(?:"|&quot;)(.*?)(?:"|&quot;)/ism', '$1=%%dquote%%$2%%dquote%%', $string);
+			$string = preg_replace_callback('/((?:href|url|rel|target|title|text)=(?:%%dquote%%|%%squote%%)(?:.*?)(?:%%dquote%%|%%squote%%))/ism', 'add_link_to_caption_replace_quotes', $string);
+			$string = preg_replace_callback('/\}(.*?)\{\/link\}/ism', 'add_link_to_caption_replace_quotes2', $string);
+			$caption = preg_match('/caption(?:\s+)?\=(?:\s+)?(?:\'|"|&quot;|&#39;)(.*?)(?:\'|"|&quot;|&#39;)/ism', $string, $m);
+			$caption = $m[1];
+		}
+	}
 	extract(shortcode_atts(array(
 		'id'	=> '',
 		'align'	=> 'alignnone',
 		'width'	=> '',
-		'caption' => ''
+		'caption' => $caption
 	), $attr));
 
 	# BEGIN Added for this plugin
 		// replaces {link rel="nofollow" url="http://www.example.com"}Text{/link}
 		$caption = preg_replace('/\{link(.*?)\}(.*?)\{\/link\}/ism', '[add_caption_link$1]$2[/add_caption_link]', $caption);
-		
+
 		// Added for this plugin it replaces {link rel="nofollow" url="http://www.example.com" text="Text" /}
 		$caption = preg_replace('/\{link(.*?)\/\}/ism', '[add_caption_link $1 /]', $caption);
-		
 		$caption = str_replace('&quot;', '"', $caption);
-	# END Added for this plugin
 
+	# END Added for this plugin
 	if ( 1 > (int) $width || empty($caption) )
-		return $content;
+		return add_link_to_caption_replace_quotes($content, 'remove');
 
 	if ( $id ) $id = 'id="' . esc_attr($id) . '" ';
-	
+			
 	// Added do_shortcode() to the $caption for this plugin
 	return '<div ' . $id . 'class="wp-caption ' . esc_attr($align) . '" style="width: ' . (10 + (int) $width) . 'px">'
-	. do_shortcode( $content ) . '<p class="wp-caption-text">' . do_shortcode($caption) . '</p></div>';
+	. do_shortcode( $content ) . '<p class="wp-caption-text">' . add_link_to_caption_replace_quotes(do_shortcode($caption), 'remove') . '</p></div>';
 }
-
-add_shortcode('add_caption_link', 'add_link_to_caption_shortcode_shortcode');
 function add_link_to_caption_shortcode_shortcode($attr, $content = null) {
+	foreach($attr as $k => $v) {
+		if($k != 'text' && $k != 'title') {
+			$attr[$k] = str_replace('%%squote%%', '', str_replace('%%dquote%%', '', str_replace('"','', str_replace('&#39;', '', str_replace('&quot;', '', str_replace('\'', '', $v))))));
+		}
+	}
 	extract(shortcode_atts(array(
 		'url'		=> '',
 		'href'		=> '',
 		'target'	=> '',
+		'title'	=> '',
 		'rel'		=> '',
 		'text'		=> ''
 	), $attr));
 	
-	$url = empty($url) ? '' :  ' href="'.$url.'"';
-	$href = empty($url) ? ' href="'.$url.'"' : $url; // We'll also allow $href to be used if $url is empty.
+	$text =  empty($text) ? '' : add_link_to_caption_replace_quotes($text, 'remove');
+	$content = empty($content) ? '' : add_link_to_caption_replace_quotes($content, 'remove');
+	$title = empty($title) ? '' : add_link_to_caption_replace_quotes($title, 'remove');
+	
+	if(!empty($url)) {
+		$link = ' href="'.$url.'"';
+	} elseif(!empty($href)) {
+		$link = ' href="'.$href.'"';
+	}
+	
 	$rel = empty($rel) ? '' :  ' rel="'.$rel.'"';
 	$target = empty($target) ? '' :  ' target="'.$target.'"';
+	$title = empty($title) ? '' :  ' title='.$title.'';
 	
-	if(empty($url) || (empty($content) && empty($text))) { return $content; }
+	if(empty($link) || (empty($content) && empty($text))) { return $content; }
 	else if(empty($content)) { $content = $text; }
 	
-	return "<a{$href}{$target}{$rel}>$content</a>";
+	return "<a{$link}{$target}{$title}{$rel}>$content</a>";
 }
+
+
+?>
--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 === Links in Captions ===
 Tags: captions, links, link, link in captions, caption, image, images, editor, rich text editor, text editor, html editor
 Requires at least: 2.5
-Tested up to: 3.0.2
+Tested up to: 3.0.3
 Stable tag: trunk
 Contributors: katzwebdesign
 Donate link:https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=zackkatz%40gmail%2ecom&item_name=Links%20in%20Captions&no_shipping=0&no_note=1&tax=0&currency_code=USD&lc=US&bn=PP%2dDonationsBF&charset=UTF%2d8
@@ -19,7 +19,7 @@ With this plugin, you can also <strong>use <a href="http://codex.wordpress.org/S
 <h4>How to use</h4>
 Inside the <code>caption</code> input, you can now add a link by using the following format: <code>{link url="http://www.example.com"}Anchor text here{/link}</code>
 
-Optional: You can also specify <code>target</code> and <code>rel</code> attributes: <code>{link url="http://www.example.com" target="_blank" rel="nofollow"}Anchor text here{/link}</code>
+Optional: You can also specify <code>target</code>, <code>title</code>, and <code>rel</code> attributes: <code>{link url="http://www.example.com" target="_blank" rel="nofollow" title="I never could do this before!"}Anchor text here{/link}</code>
 
 > ####You may also be interested in:
 > <strong><a href="http://wordpress.org/extend/plugins/rich-text-tags/">Rich Text Tags</a></strong> - Enable rich text editing of tags, categories, and taxonomies. Add value to your tag & category pages and improve your website' SEO.
@@ -40,10 +40,18 @@ Optional: You can also specify <code>target</code> and <code>rel</code> attribut
 
 == Changelog ==
 
+= 1.1 = 
+* Fixed issue with quotes: I did a ton of variations and possible combinations and made sure that each combination works. Now it shouldn't matter whether the caption uses single or double quotes, or even any attributes inside the link
+* Added `title` attribute support
+
 = 1.0 =
 * Initial plugin release.
 
 == Upgrade Notice ==
+
+= 1.1 =
+* Fixed issue with quotes: I did a ton of variations and possible combinations and made sure that each combination works. Now it shouldn't matter whether the caption uses single or double quotes, or even any attributes inside the link
+* Added `title` attribute support
 
 = 1.0 = 
 * Let's go linking!
